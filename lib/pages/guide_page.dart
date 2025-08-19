f7514df6b10b01d3a981d784f8e59143e19bb503
vvd <support@vvdhealth.com>
2025-08-19 11:58:03 +0900

    -

--- lib/pages/guide_page.dart
+++ lib/pages/guide_page.dart
@@ -85,7 +85,7 @@ class _GuidePageState extends State<GuidePage> {
                   padding: EdgeInsets.symmetric(vertical: 16),
                 ),
                 child: Text(
-                  '커뮤니티 인기글 더보기',
+                  '다이어트 주사 병원,약국 찾기',
                   style: T1_Bold.copyWith(
                     color: BLUE_50,
                   ),
--- lib/pages/prescription_page.dart
+++ lib/pages/prescription_page.dart
@@ -97,7 +97,7 @@ class _PrescriptionPageState extends State<PrescriptionPage> {
                   padding: EdgeInsets.symmetric(vertical: 16),
                 ),
                 child: Text(
-                  '다이어트 주사 병원,약국 찾기',
+                  '커뮤니티 인기글 더보기',
                   style: T1_Bold.copyWith(
                     color: BLUE_50,
                   ),
