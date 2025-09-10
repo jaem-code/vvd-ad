9f8f07a7f1914dcffcf1e1044e0a85b0ba053527
vvd <support@vvdhealth.com>
2025-09-10 14:13:40 +0900

    - handok page

--- lib/pages/handok_page.dart
+++ lib/pages/handok_page.dart
@@ -40,31 +40,37 @@ class _HandokPageState extends State<HandokPage> {
                           width: double.infinity,
                           child: CachedNetworkImage(
                               imageUrl:
-                                  'https://images.vvd-health.com/back-office/admin/banner/5f78cdcc-4cc7-4e5b-a85f-b643a1d5b20a-719e0890-3d2f-418b-9d38-819b22afad20.png'),
+                                  'https://images.vvd-health.com/back-office/admin/banner/f21d3c1e-475d-4dec-af3a-e6942ef96803-c0e0cf60-8d72-4249-9686-1f8a4c6b1455.png'),
                         ),
                         SizedBox(
                           width: double.infinity,
                           child: CachedNetworkImage(
                               imageUrl:
-                                  'https://images.vvd-health.com/back-office/admin/banner/296a8201-5dfe-495a-99b1-82e2011f69e9-d8d8e545-0f91-40f2-883e-a514ce87c532.png'),
+                                  'https://images.vvd-health.com/back-office/admin/banner/5ac3fd99-e04f-4a1a-a4ad-95e8e14af362-d857d951-52f4-4f7e-85d5-57a76c2f15de.png'),
                         ),
                         SizedBox(
                           width: double.infinity,
                           child: CachedNetworkImage(
                               imageUrl:
-                                  'https://images.vvd-health.com/back-office/admin/banner/6df8d359-3fdb-43cc-b508-8eada8616639-8c24676f-613c-4513-8972-73c20f79d9b2.png'),
+                                  'https://images.vvd-health.com/back-office/admin/banner/75209e5d-31d4-4dfb-b208-25ebbd979b4f-8798c76c-55ca-4b65-9960-bdaf1f1e3b09.png'),
                         ),
                         SizedBox(
                           width: double.infinity,
                           child: CachedNetworkImage(
                               imageUrl:
-                                  'https://images.vvd-health.com/back-office/admin/banner/be8876da-7786-44b2-9858-19d81a126fd1-1fc8aef1-abf6-4aba-93f6-5295c08719d5.png'),
+                                  'https://images.vvd-health.com/back-office/admin/banner/be56d516-ee87-41b4-9c55-74de4c604b27-1488d87f-6b57-42f5-961e-9f53338d95db.png'),
                         ),
                         SizedBox(
                           width: double.infinity,
                           child: CachedNetworkImage(
                               imageUrl:
-                                  'https://images.vvd-health.com/back-office/admin/banner/fa619141-9f61-4346-b87e-57145c91341e-232a8cfd-993e-4e21-93de-08d0c37f0b39.png'),
+                                  'https://images.vvd-health.com/back-office/admin/banner/aadb6516-a177-438e-9770-670161547075-e114b751-a876-47ff-9df3-29065939f328.png'),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: CachedNetworkImage(
+                              imageUrl:
+                                  'https://images.vvd-health.com/back-office/admin/banner/d641fea5-4d37-42c0-992b-3b423eb8ecdd-04d0da0a-32d6-4aea-a44f-4a5e376e15ba.png'),
                         ),
                         SizedBox(height: 40),
                       ],
